28c939bc0814f531ac86188a299a1c80383fd903
Niyatha <niyatha@ceino.com>
2016-07-13 11:28:55 +0530

    first commit

---
+++ JavaTest.java
@@ -0,0 +1,16 @@
+
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		 
+if(args.length > 0)
+{
+		System.out.println("Hello");
+for(int i =0 ;i<=args.length(); i++)
+{
+System.out.println("args "+i+":"+args[0]);
+}
+}
+	}
+
+}
